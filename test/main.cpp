a59aa5bbd55fbff3c8d3ce3ca0c0a654b60c199a
Alaisdair West <alaisdair.west@outlook.com.au>
2024-05-05 23:26:11 +1000

    Remove attempt at navigation meshes

--- test/main.cpp
+++ test/main.cpp
@@ -7,12 +7,9 @@
 #include <iostream>
 #include <proxima/proxima.hpp>
 #include <proxima/simplegrid.hpp>
-#include <proxima/mesh.hpp>
-#include <polypartition/polypartition.h>
-#include <list>
 
 int main()
-{/*
+{
     proxima::SimpleGrid<uint8_t> grid (10, 10);
     grid.fill(1);
 
@@ -46,34 +43,6 @@ int main()
             if (j != grid.width - 1) std::cout << ",";
         }
         std::cout << "\n";
-    }*/
-
-    std::vector<proxima::Vertex> border (4);
-    border[0].x = 0.0f;
-    border[0].y = 0.0f;
-    border[1].x = 1.0f;
-    border[1].y = 0.0f;
-    border[2].x = 1.0f;
-    border[2].y = -1.0f;
-    border[3].x = 0.0f;
-    border[3].y = -1.0f;
-    proxima::Polygon poly (border);
-    poly.startIsland(3);
-    poly.pushIslandVertex({ 0.25, -0.25 });
-    poly.pushIslandVertex({ 0.75, -0.75 });
-    poly.pushIslandVertex({ 0.25, -0.75 });
-    poly.finishIsland();
-    proxima::Mesh mesh = proxima::GenerateMeshFromPolygon(poly);
-
-    for (uint16_t i = 0; i < mesh.vertexCount(); i++)
-    {
-        std::cout << "[ " << i << " ] ( " << mesh[i].x << ", " << mesh[i].y << " )\n";
     }
-    for (uint16_t i = 0; i < mesh.triangleCount(); i++)
-    {
-        std::cout << "{ " << mesh(i, 0) << ", " << mesh(i, 1) << ", " << mesh(i, 2) << " }\n";
-    }
-
-
     return 0;
 }
